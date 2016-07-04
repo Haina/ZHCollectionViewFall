c2f10cf507152eb3fd68160f17a26b8f9ba0e626
Haina <uniqueZFJ@gmail.com>
2016-07-04 14:48:32 +0800

    CollectionViewFall

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+link_with ‘ZHCollectionViewFall’
+
+platform :ios, '6.0'
+    
+pod 'MJRefresh'
---
+++ ZHCollectionViewFall/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZHCollectionViewFall
+//
+//  Created by 张凤娟 on 16/6/30.
+//  Copyright © 2016年 张凤娟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
